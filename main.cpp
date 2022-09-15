8cf7ba0500f77af9ce2a24dd50d12f92185fe23e
seyfaluic <112576150+seyfaluic@users.noreply.github.com>
2022-09-14 23:09:15 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  Project 1 Starter Code - DNA Profiling
+//  Created by Seyfal Sultanov on 09/14/2022
+//  Mac OS 12.2.1 (21D74) - VS Code 1.71.1 
+//
+
+#include "ourvector.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    
+    return 0;
+}
+ // bruh when will it work 
